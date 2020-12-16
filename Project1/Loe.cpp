346baf1e32944b2e73b30a0c5da2583be7548b2f
QUỲNH THI PHẠM <thipham0502@gmail.com>
2020-12-16 10:57:11 +0700

    Bai 11, 13

---
+++ Project1/Loe.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <stdlib.h>
+//#include <math.h>
+//using namespace std;
+//int main()
+//{
+//    // TODO
+//    int n, i;
+//    double num[100], max;
+//    cin >> n;
+//
+//    if (n > 0)
+//    {
+//        for (i = 0; i < n; i++)
+//        {
+//            cin >> num[i];
+//        }
+//        max = num[0];
+//        for (i = 1; i < n; i++)
+//        {
+//            if (max < num[i])
+//                max = num[i];
+//        }
+//        cout << fixed << setprecision(2) << max;
+//    }
+//    return 0;
+//}
---
+++ Project1/Mo.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<iomanip>
+#include<stdlib.h>
+
+using namespace std;
+int main()
+{
+    // TODO
+    int n, i, j, a[100][100], t[100][100];
+    cin >> n;
+    if (n > 0)
+    {
+        // input a
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                cin >> a[i][j];
+
+        // print a
+        //cout << "a:\n";
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (j == n - 1)
+                    cout << a[i][j];
+                else
+                    cout << a[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        // find t (tranpose)
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                t[j][i] = a[i][j];
+        
+        // print t
+        //cout << "transpose:\n";
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (j == n - 1)
+                    cout << t[i][j];
+                else
+                    cout << t[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+        return 0;
+}
